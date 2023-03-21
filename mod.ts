cc4ba939c2a77931ab98d56341f497b05ec9382d
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-21 23:45:02 +0900

    feat(mod): export strategy types

--- mod.ts
+++ mod.ts
@@ -1,11 +1,6 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export {
-  type ETag,
-  type ETagFormat,
-  type Handler,
-  type Middleware,
-} from "./deps.ts";
+export { type Handler, type Middleware } from "./deps.ts";
 export { etag } from "./middleware.ts";
-export type { CalculateETag } from "./types.ts";
+export type { Algorithm, ETagStrategy } from "./types.ts";
