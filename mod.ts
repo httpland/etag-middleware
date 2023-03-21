3547003819adad73b267dbaffe4b9e3ae26161a3
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-21 10:25:45 +0900

    feat(mod): export etag interface

--- mod.ts
+++ mod.ts
@@ -1,6 +1,11 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { type Handler, type Middleware } from "./deps.ts";
+export {
+  type ETag,
+  type ETagFormat,
+  type Handler,
+  type Middleware,
+} from "./deps.ts";
 export { etag } from "./middleware.ts";
-export type { CalculateETag, ETag } from "./types.ts";
+export type { CalculateETag } from "./types.ts";
